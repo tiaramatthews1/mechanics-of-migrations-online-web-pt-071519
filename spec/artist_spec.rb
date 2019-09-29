71b8551154b1c4ef9c47231abad5eb9a99108d33
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-29 11:07:50 +0000

    Automatically backed up by Learn

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -3,11 +3,11 @@ require_relative './spec_helper'
 describe "Artist" do
   let(:artist) {Artist.new(name: 'Justice', genre: 'Electronic', age: Time.now.strftime("%Y").to_i - 2003, hometown: "Paris")}
 
-  xit "can instantiate a new instance" do
+  it "can instantiate a new instance" do
     expect(Artist.new.is_a?(Object)).to eq(true)
   end
 
-  xit "can instantiate with a name, genre, age and hometown" do
+  it "can instantiate with a name, genre, age and hometown" do
 
     expect(artist.name).to eq("Justice")
     expect(artist.genre).to eq("Electronic")
@@ -15,19 +15,19 @@ describe "Artist" do
     expect(artist.hometown).to eq("Paris")
   end
 
-  xit "can be saved to the database" do
+  it "can be saved to the database" do
     artist.genre = "Electronic"
 
     expect(artist.save).to eq(true)
   end
 
-  xit "can instantiate and save at the same time with create" do
+  it "can instantiate and save at the same time with create" do
     artist = Artist.create(name: 'Justice', genre: "Electronic", age: Time.now.strftime("%Y").to_i - 2003, hometown: "Paris")
 
     expect(Artist.all.last.name).to eq("Justice")
   end
 
-  xit "can find an Artist by name" do
+  it "can find an Artist by name" do
     weeknd = Artist.create(name: 'The Weeknd', genre: "Alternative R&B", age: Time.now.strftime("%Y").to_i - 2010, hometown: "Toronto")
     taytay = Artist.create(name: 'Taylor Swift', genre: "Pop / Country", age: Time.now.strftime("%Y").to_i - 2006, hometown: "Reading")
     queen = Artist.create(name: 'Queen', genre: "Rock", age: Time.now.strftime("%Y").to_i - 1973, hometown: "London")
@@ -35,7 +35,7 @@ describe "Artist" do
     expect(Artist.find_by(name: "Taylor Swift")).to eq(taytay)
   end
 
-  xit "can roll back to have no favorite_food attribute for Artist" do    
+  it "can roll back to have no favorite_food attribute for Artist" do    
     expect(Artist.column_names).to eq(["id", "name", "genre", "age", "hometown"])
   end
 end
